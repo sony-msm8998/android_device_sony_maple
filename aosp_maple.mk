08625e8d82fb56d8e2b79ab12d628c9cf8978ece
Shujath <mmfshujath@gmail.com>
2020-05-10 14:14:06 +0530

    Update aosp_maple.mk

--- aosp_maple.mk
+++ aosp_maple.mk
@@ -10,8 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 ### BOOTANIMATION
 # vendor/lineage/config/common_full_phone.mk
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
+#TARGET_SCREEN_HEIGHT := 1920
+#TARGET_SCREEN_WIDTH := 1080
 # vendor/lineage/config/common.mk
 TARGET_BOOTANIMATION_HALF_RES := true
 
